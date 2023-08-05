9ee396c51458654d7046b19ab9dd33141d7f7161
Nehal Patel <iamnehalpatel@gmail.com>
2023-08-05 16:34:01 +0530

    state instance demo placeholders.

--- app/src/main/java/in/nehalpatel/sdjickotlin/SaveInstanceStateActivity.kt
+++ app/src/main/java/in/nehalpatel/sdjickotlin/SaveInstanceStateActivity.kt
@@ -2,10 +2,40 @@ package `in`.nehalpatel.sdjickotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class SaveInstanceStateActivity : AppCompatActivity() {
+    private var tvCounter:TextView? = null
+    private var counter:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_save_instance_state)
+
+        tvCounter = findViewById<TextView>(R.id.tvCounter)
     }
+
+    fun decreseCounter(view: View) {
+        counter--
+        tvCounter?.text = counter.toString()
+    }
+    fun increaseCounter(view: View) {
+        counter++
+        tvCounter?.text = counter.toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("counter", counter)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        counter = savedInstanceState.getInt("counter")
+        tvCounter?.text = counter.toString()
+    }
+
+    fun onClickSaveInstance(view: View) {}
 }
